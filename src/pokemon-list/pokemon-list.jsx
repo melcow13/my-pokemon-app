6495450ea8ca8281584db010c45295f5a669ce9b
Melody Kao <melody.ch.kao@gmail.com>
2022-05-17 09:31:10 +0200

    Remove all unnecessary files

---
+++ src/pokemon-list/pokemon-list.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PokemonList extends React.Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className='pokemon-list'>
+                
+            </div>
+        );
+    }
+}
+ 
+export default PokemonList;
