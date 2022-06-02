be1abd1bb23c9d733f318374b2262a6479e177b0
Melody Kao <melody.ch.kao@gmail.com>
2022-06-02 15:37:36 +0200

    add Modal to PokemonCard

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,38 +1,71 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Button, Card, CardGroup } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Button, Card, CardGroup, Container, Modal, Image } from 'react-bootstrap';
 import './PokemonCard.scss';
 
 
 
 export default function PokemonCard({ pokemonList }) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const [pokemon, setPokemon] = useState(null)
+  const [pokemon, setPokemon] = useState(null);
+  const [pokemonAbilities, setPokemonAbilities] = useState(null);
+  const [pokemonTypes, setPokemonTypes] = useState(null);
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonList}`).then(res => {
       setPokemon(res.data)
+      setPokemonAbilities(res.data.abilities)
+      setPokemonTypes(res.data.types)
     })
 
   }, [])
 
+
+
+
+
   if (!pokemon) return <div>loading...</div>
   console.log(pokemon)
   return (
-    <CardGroup >
-      <Card style={{ width: '18rem' }} md={12}>
-        <Card.Header as="h2">{pokemon.name}</Card.Header>
-        <Card.Body>
-          <Card.Img variant="top" width={250} height={250} src={pokemon.sprites.other.dream_world.front_default} />
-        </Card.Body>
-        <Card.Footer>
-          <Link to={`/pokemon/${pokemonList}`}>
-            <Button variant="primary">
+    <>
+      <CardGroup >
+        <Card style={{ width: '18rem' }} md={12}>
+          <Card.Header as="h2">{pokemon.name}</Card.Header>
+          <Card.Body>
+            <Card.Img variant="top" width={250} height={250} src={pokemon.sprites.other.dream_world.front_default} />
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="primary" onClick={handleShow}>
               View More
             </Button>
-          </Link>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+
+      <Modal show={show} onHide={handleClose} size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header>
+          <Modal.Title>{pokemon.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container >
+            <Image width={200} height={200} src={pokemon.sprites.front_shiny} />
+            <Image width={200} height={200} src={pokemon.sprites.back_shiny} />
+            <br />
+            <h4>Weight: </h4> <p>{pokemon.weight} </p>
+            <h4>Abilities: </h4> {pokemonAbilities.map(a => <li key={a}>{a.ability.name}</li>)}
+            <h4>Type: </h4>{pokemonTypes.map(t => <li key={t}>{t.type.name}</li>)}
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
--- src/PokemonDetails.jsx
+++ src/PokemonDetails.jsx
@@ -1,37 +1,39 @@
 import axios from 'axios';
-import React, {  useEffect, useState } from 'react'
-import { Card, CardGroup} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react'
+import { Modal, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 
 export default function PokemonDetails() {
   const [pokemonDetails, setPokemonDetails] = useState(null);
-  const {name} = useParams()
- 
+  const { name } = useParams()
+
 
   useEffect(() => {
     // const pokemon = pokemonList.find( p=>p ===params.name);
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then(res=>{
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then(res => {
       setPokemonDetails(res.data)
     })
-      
+
   }, [])
-  
-   
-if (!pokemonDetails) return <div>loading...</div>
-  
-    
+
+
+  if (!pokemonDetails) return <div>loading...</div>
+
+
   return (
-    <CardGroup>
-      <Card>
-      <Card.Title>
-            Pokemon Ability
-      </Card.Title>
-        
-        <Card.Body>
-          <div>{pokemonDetails.base_experience}</div>
-        </Card.Body>
-      </Card>
-    </CardGroup>  
+    <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>{pokemonDetails.name}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p>Pokemon Ability: </p>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary">Close</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
   )
 }
