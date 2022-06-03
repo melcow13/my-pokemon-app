d41f2f4f2fa5b29966b783f6fb246ac59b3a890a
Melody Kao <melody.ch.kao@gmail.com>
2022-06-03 13:54:20 +0200

    update stylesheet

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Image, Container, Row, Col } from 'react-bootstrap'
 import logo from './img/pokemon-logo.png';
+import "./WelcomePage.css";
 
 export default function WelcomePage() {
   return (
     <Container className='welcomecontainer'>
       <Row>
-        <Image variant="center" src={logo} />
-        <Col as="h1">
-          WELCOME TO POKEMON APP
+        <Col>
+        <Image variant="center" width="900" height="400" src={logo} />
         </Col>
+        <Col as="h1">
+          WELCOME TO POKEDEX
+        </Col>
+        <Col>
         <Link to={`/pokemon`}>
           <Button>
             View Pokemon
           </Button>
         </Link>
+        </Col>
       </Row>
     </Container>
 
